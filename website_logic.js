935351fb9234aea47367542f2569b34d78c99b4d
John Janigan-Mills <cosmicmonkeysounds@gmail.com>
2020-11-26 18:33:57 -0400

    more hiding logic, need to refactor

--- website_logic.js
+++ website_logic.js
@@ -25,8 +25,34 @@ function topBarClicked(name)
         }
     }
 
-    console.log(topBarButtonsClicked);
+}
+
+function portfolioSort(kind)
+{
+    let galleryChildren = document.getElementsByClassName(kind);
+
+    document.getElementById(kind.concat("_btn")).style.textDecoration = "underline";
+
+    for( const [key, value] of Object.entries(document.getElementById("gallery_bar").getElementsByTagName('a')) )
+    {
+        if( value.id != kind.concat("_btn") )
+        {
+            document.getElementById(value.id).style.textDecoration = "none";
+        }
+    }
+
+    for( const [key, value] of Object.entries(document.getElementsByClassName("all")) )
+    {
+        value.style.display = "none";
+    }
+
+    for( const [key, value] of Object.entries(galleryChildren) )
+    {
+        value.style.display = "inline";
+    }
+
 
 }
 
 topBarClicked("portfolio");
+portfolioSort("all");
